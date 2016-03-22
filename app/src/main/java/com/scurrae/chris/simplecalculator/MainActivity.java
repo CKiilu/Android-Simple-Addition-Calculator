62dc421e48a2afa935c073ff9a4b22ac646e0234
CKiilu <cndeti@gmail.com>
2016-03-22 14:56:03 +0300

    Replaced calculations with modular functions

--- app/src/main/java/com/scurrae/chris/simplecalculator/MainActivity.java
+++ app/src/main/java/com/scurrae/chris/simplecalculator/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int num1;
+    private int num2;
+    private TextView t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,38 +27,40 @@ public class MainActivity extends AppCompatActivity {
     public void onButtonClick(View v){
         EditText e1 = (EditText)findViewById(R.id.editText);
         EditText e2 = (EditText)findViewById(R.id.editText2);
-        final TextView t2 = (TextView)findViewById(R.id.textView);
-        final int num1 = Integer.parseInt(e1.getText().toString());
-        final int num2 = Integer.parseInt(e2.getText().toString());
+        t2 = (TextView)findViewById(R.id.textView);
+        num1 = Integer.parseInt(e1.getText().toString());
+        num2 = Integer.parseInt(e2.getText().toString());
         Button b1 = (Button)findViewById(R.id.button);
         Button b2 = (Button)findViewById(R.id.button2);
         Button b3 = (Button)findViewById(R.id.button3);
         Button b4 = (Button)findViewById(R.id.button4);
 
-        b1.setOnClickListener(new View.OnClickListener(){
-            public  void onClick(View v){
-                int sum = num1 + num2;
-                t2.setText(String.valueOf(sum));
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                int sum = num1 - num2;
-                t2.setText(String.valueOf(sum));
-            }
-        });
-        b3.setOnClickListener(new View.OnClickListener(){
-            public  void onClick(View v){
-                int sum = num1 * num2;
-                t2.setText(String.valueOf(sum));
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener(){
-            public  void onClick(View v){
-                int sum = (num1 / num2);
-                t2.setText(String.valueOf(sum));
-            }
-        });
+        Button[] b = {b1,b2,b3,b4};
+        for(Button bt:b){
+            bt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch(v.getId()){
+                        case R.id.button:
+                            getSum();
+                            break;
+                        case R.id.button2:
+                            getDiff();
+                            break;
+                        case R.id.button3:
+                            getProd();
+                            break;
+                        case R.id.button4:
+                            getQuo();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            });
+        }
+
+
     }
 
     @Override
@@ -78,5 +83,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public int getSum(){
+        int sum = num1 + num2;
+        t2.setText(String.valueOf(sum));
+        return sum;
+    }
+    public int getDiff(){
+        int difference = num1 - num2;
+        t2.setText(String.valueOf(difference));
+        return difference;
+    }
+    public int getProd(){
+        int product = num1 * num2;
+        t2.setText(String.valueOf(product));
+        return product;
+    }
+    public int getQuo(){
+        int quotient = num1 / num2;
+        t2.setText(String.valueOf(quotient));
+        return quotient;
     }
 }
